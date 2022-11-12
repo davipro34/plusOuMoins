f5807bd05385a5db9b42129f50effb32af0f76db
David <david@davipro.fr>
2022-11-12 13:02:24 +0100

    Entetes de base du fichier main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+// main.c
+//
+// Program's name : plusOuMoins
+//
+// Created by David Atlan on 11/12/2022
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+
+        return 0;
+}
